5840f4f872717f86d968311156e69838b463f247
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-09-19 23:03:37 +0545

    Binary search question + bubble sort completed

--- Day6 Binary Search/src/com/gaurab/RotationCount.java
+++ Day6 Binary Search/src/com/gaurab/RotationCount.java
@@ -1,52 +1,16 @@
-public class RBS {
-    // https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+public class RotationCount {
     public static void main(String[] args) {
         int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(findPivot(arr));
-    }
-    static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-
-        // if you didnt find the pivot, it means the array is not rotated
-        if(pivot == -1){
-            return binarySearch(nums, target, 0, nums.length -1);
-        }
-
-        // if pivot is found you have found two ascending sorted array
-
-        if(nums[pivot] == target){
-            return pivot;
-        }
-
-        if (target>=nums[0]){
-            return binarySearch(nums,target, 0, pivot-1);
-        }
-
-        return binarySearch(nums, target, pivot+1, nums.length -1);
-
-
+        System.out.println(countRotations(arr));
     }
 
-    static int binarySearch(int[] arr, int target, int start , int end){
+     static int countRotations(int[] arr) {
+        int pivot = findPivot(arr);
 
-        while(start<=end){
-            //find the middle element
-            // int mid = (start + end)/2; // might be possibe that (start + end) exceed the range of integer in java
-            int mid = start + (end - start)/2;
-
-            if(target < arr[mid]){
-                end = mid -1;
-            }else if(target > arr[mid]){
-                start = mid + 1;
-            } else{
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
+        return pivot + 1;
     }
 
-    // this will not work for duplicate values
+    // for non duplicates
     static int findPivot(int[] arr){
         int start = 0;
         int end = arr.length - 1;;
@@ -67,6 +31,9 @@ public class RBS {
         }
         return -1;
     }
+
+
+    // for duplicates
 
     static int findPivotWithDuplicates(int[] arr){
         int start = 0;
@@ -103,7 +70,7 @@ public class RBS {
             // left side is sorted, so pivot should be in rght
             else if ((arr[start]< arr[mid]) ||  (arr[start] == arr[mid] && arr[mid] > arr[end])) {
                 start = mid + 1;
-                
+
             }else {
                 end = mid - 1;
             }
---
+++ Day6 Binary Search/src/com/gaurab/SortedMatrix.java
@@ -0,0 +1,22 @@
+public class SortedMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] binarySearch(int [] matrix, int row, int cS, int cE, int target){
+        while(cS<cE){
+            int mid = cS + (cE-cS)/2;
+            if()
+        }
+    }
+    static int[] search(int[][] matrix, int target){
+
+
+         int rows = matrix.length -1;
+         int cols = matrix[0].length; // be cautious matrix may be empty;
+        // add a chck for that
+
+        if(rows == 1)
+
+    }
+}

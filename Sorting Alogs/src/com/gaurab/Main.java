ad0d25ffd63d2ce091fd9b7717e57c1be9314f42
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-09-20 10:12:08 +0545

    Selection Sort

--- Sorting Alogs/src/com/gaurab/Main.java
+++ Sorting Alogs/src/com/gaurab/Main.java
@@ -4,9 +4,36 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5,4,9,2,1};
-        bubbleSort(arr);
+        int[] arr = {5,4,9,2,55,77,12,98,1};
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr){
+        for (int i = 0; i<arr.length;i++){
+            // find the maximum item in the remaiing array and swap with correct index
+            int last = arr.length -i -1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr,maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int last){
+        int max = start;
+
+        for (int i = start; i<=last;i++){
+            if (arr[max]<arr[i]){
+                max = i;
+            }
+        }
+
+        return max;
     }
 
     static void bubbleSort(int[] arr){

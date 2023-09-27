0a15cb152a91ab21efaeecd0439dca2b332d687d
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-09-27 13:57:24 +0545

    Missing number

--- Sorting Alogs/src/com/gaurab/CyclicSort.java
+++ Sorting Alogs/src/com/gaurab/CyclicSort.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {3,5,2,1,4};
-        cyclicSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {3,6,0,2,1,4};
+        missingNumber(arr);
+        System.out.println(missingNumber(arr));
     }
     static void cyclicSort(int[] arr){
         int i = 0;
@@ -24,4 +24,19 @@ public class CyclicSort {
         arr[a] = arr[b];
         arr[b] = tmp;
     }
+
+    // Amazon Question [0,N]
+    // Using mathematical approach
+    static int missingNumber(int[] nums) {
+        int actualSum = nums.length*(nums.length+1)/2;
+        return actualSum - sum(nums);
+    }
+   static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0;i<arr.length;i++){
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
 }

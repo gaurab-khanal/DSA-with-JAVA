64f9a5ccb99d7047e43a940b2a07d5b6acb77947
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-09-27 12:52:43 +0545

    Cyclic Sort

---
+++ Sorting Alogs/src/com/gaurab/CyclicSort.java
@@ -0,0 +1,27 @@
+package com.gaurab;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correctIndex = arr[i] -1;
+            if (arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }else {
+                i++;
+            }
+        }
+    }
+    static void swap(int arr[], int a, int b){
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}

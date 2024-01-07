e0b15b314f58b665818058a0a66e96aa3cfe014f
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-01-07 11:47:19 +0545

    insertion sort

--- Day9 Sorting/src/com/gaurab/Main.java
+++ Day9 Sorting/src/com/gaurab/Main.java
@@ -4,9 +4,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5, 9, 20,15};
-        selectionSort(arr);
+        int[] arr = {5,3,4,1,-2};
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] arr){
+        for (int i = 0; i<=arr.length -2; i++){
+            for (int j= i + 1; j>0;j--){
+                if (arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else {
+                    break;
+                }
+            }
+        }
     }
 
     static void selectionSort(int[] arr){

fcebbd02b16748c53b7826bbabf2d7c29fb105e3
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-07-12 08:32:53 +0545

    bs 2d

--- Day6 Binary Search/src/com/gaurab/BS2D.java
+++ Day6 Binary Search/src/com/gaurab/BS2D.java
@@ -9,32 +9,27 @@ public class BS2D {
                 {28, 29, 37, 49},
                 {33, 34, 38, 50}
         };
-
-        int row = 0;
         int target = 37;
-        for(int i = 0; i<arr.length; i++){
-            if(target<=arr[i][arr[i].length-1] && target>= arr[i][0]){
-                row = i;
-            }
-        }
 
-        int[] ans = BS(arr, target, 0, arr[row].length-1, row );
+        int[] ans = BS(arr, target );
 
         System.out.println(Arrays.toString(ans));
 
     }
 
-    static int[] BS(int[][] arr, int target, int start, int end,int row){
+    static int[] BS(int[][] arr, int target){
 
-        while (start<=end){
-            int mid  = start + (end-start)/2;
+        int row = 0;
+        int col = arr.length -1;
 
-            if(target  > arr[row][mid]){
-                start = mid +1;
-            }else if(target< arr[row][mid]){
-                end = mid -1;
+        while (row< arr.length && col > 0){
+
+            if(target  > arr[row][col]){
+                row++;
+            }else if(target< arr[row][col]){
+                col--;
             }else{
-                return new int[] {row, mid};
+                return new int[] {row, col};
             }
 
         }
--- Day6 Binary Search/src/com/gaurab/Sorted2d.java
+++ Day6 Binary Search/src/com/gaurab/Sorted2d.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Sorted2d {
     public static void main(String[] args) {
         int[][] arr = {
@@ -6,16 +9,83 @@ public class Sorted2d {
                 {9,10,11,12},
                 {13,14,15,16}
         };
-        int target = 10;
-    }
-    static int[][] BS(int[][] arr,int target){
-        int start = arr[0][0];
-        int row = arr.length-1;
-        int col = arr[arr.length].length;
-        int end = arr[row][col];
+        int target = 17;
 
-        while(start<=end){
-            int mid =
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    // Search in the row provided betweens the cols provided
+    static  int[] binarySearch(int[][] matrix, int row, int colStart, int colEnd, int target){
+        while (colStart<=colEnd){
+            int mid = colStart + (colEnd-colStart)/2;
+            if(matrix[row][mid]==target){
+                return  new int[] {row,mid};
+            }
+            if(matrix[row][mid]<target){
+                colStart = mid +1;
+            }else {
+                colEnd = mid-1;
+            }
         }
+        return  new int[] {-1,-1};
+    }
+    static int[] search(int[][] matrix, int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if(rows==0 || cols==0) {
+            return new int[]{-1,-1};
+        }
+
+        if(rows == 1){
+            return binarySearch(matrix, 0, 0, cols-1, target);
+        }
+
+        int rowStart = 0;
+        int rowEnd = rows-1;
+        int midCol = cols/2;
+
+        // run the loop till two rows are remaining
+        while (rowStart < (rowEnd-1)) { // while this is true it will have more than two rows
+            int mid = rowStart + (rowEnd - rowStart) / 2;
+            if (matrix[mid][midCol] == target) {
+                return new int[]{mid, midCol};
+            }
+            if (matrix[mid][midCol] < target) {
+                rowStart = mid;
+            } else if (matrix[mid][midCol] > target) {
+                rowEnd = mid;
+            }
+        }
+
+        // Now we have two rows
+        // check whether the target is in the midCol of two rows;
+        if(matrix[rowStart][midCol] == target){
+            return  new int[] {rowStart, midCol};
+        }
+        if(matrix[rowStart+1][midCol] == target){
+            return  new  int[] {rowStart+1, midCol};
+        }
+
+        // search in 1st half
+        if(target<=matrix[rowStart][midCol-1]){
+            return  binarySearch(matrix, rowStart, 0, midCol-1 ,target);
+        }
+        // search in 2nd half
+        if(target<=matrix[rowStart+1][midCol-1]){
+            return  binarySearch(matrix, rowStart+1, 0, midCol-1,target);
+        }
+        // search in 3rd half
+        if(target>=matrix[rowStart][midCol+1] && target<= matrix[rowStart][cols-1]){
+            return  binarySearch(matrix, rowStart, midCol+1, cols-1,target);
+        }
+        // search in 4th half
+        if(target>=matrix[rowStart+1][midCol+1]){
+            return  binarySearch(matrix, rowStart+1, midCol+1, cols-1,target);
+        }
+
+
+        return  new int[]{-1,-1};
     }
 }
